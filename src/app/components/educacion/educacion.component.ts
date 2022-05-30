edfb1c3ad423f076bdfc65f734aae3a3c4473284
Rodymus <robertogonzalez.ar@gmail.com>
2022-05-30 19:03:40 -0300

    btn eliminar agregado

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducacionComponent implements OnInit {
 
+  listEducacion: any[] = [
+    { escuela: 'UNPA', titulo: 'Master en informática', descripcion: 'analista de datos', fecha: '2022'},
+    { escuela: 'UTN', titulo: 'Lic. Org. Industrial', descripcion: 'analista de costos', fecha: '2009'},
+    { escuela: '750', titulo: 'Perito Mercantil', descripcion: 'analista de contable', fecha: '1998'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  eliminarEducacion(index: number){
+    this.listEducacion.splice(index, 1);
   }
 
 }
--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienciaComponent implements OnInit {
 
+  listExperiencia: any[] = [
+    { empresa: 'UNPA', puesto: 'Master en informática', descripcion: 'analista de datos', fecha: '2022'},
+    { empresa: 'UTN', puesto: 'Lic. Org. Industrial', descripcion: 'analista de costos', fecha: '2009'},
+    { empresa: '750', puesto: 'Perito Mercantil', descripcion: 'analista de contable', fecha: '1998'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  eliminarExperiencia(index: number){
+    this.listExperiencia.splice(index, 1);
   }
 
 }
