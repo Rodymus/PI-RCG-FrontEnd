4fdd7591fa21af612c00e9df75195b5f55762cdc
Rodymus <robertogonzalez.ar@gmail.com>
2022-05-31 22:48:18 -0300

    edit agregado

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -9,11 +9,20 @@ import { educacion } from '../model/educacion.model';
 })
 export class EducacionService {
   URL = 'http://localhost:8080/educacion/';
+  EDIT ='editar/';
 
   constructor(private http: HttpClient) { }
 
   public getEducacion(): Observable<educacion>{
     return this.http.get<educacion>(this.URL + 'traer/perfil');
+  }
+
+  public deleteEducacion(): Observable<educacion>{
+    return this.http.delete<educacion>(this.URL + 'borrar/{id}');
+  }
+
+  public editEducacion(id: number, empresa: String, puesto: String, descripcion: String, fecha: String): Observable<educacion>{
+    return this.http.post<educacion>(this.URL + this.EDIT + empresa + puesto +descripcion ,fecha);
   }
 
 }
