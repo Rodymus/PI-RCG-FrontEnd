713a928a786caf277f10cc2728ceeddc23a37658
Rodymus <robertogonzalez.ar@gmail.com>
2022-07-20 19:03:27 -0300

    arreglado ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 // Import ng-circle-progress
 import { NgCircleProgressModule } from 'ng-circle-progress';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,7 +20,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import {HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { NuevoUsuarioComponent } from './model/nuevo-usuario/nuevo-usuario.component';
+//import { NuevoUsuarioComponent } from './model/nuevo-usuario/nuevo-usuario.component';
 
 
 @NgModule({
@@ -35,10 +37,11 @@ import { NuevoUsuarioComponent } from './model/nuevo-usuario/nuevo-usuario.compo
     FooterComponent,
     HomeComponent,
     LoginComponent,
-    NuevoUsuarioComponent
+    //NuevoUsuarioComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     // Specify ng-circle-progress as an import
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
   roles: string[] = [];
   errMsj!: string;
 
-  constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) {}
+  constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     if(this.tokenService.getToken()){
@@ -31,20 +31,21 @@ export class LoginComponent implements OnInit {
 
   onLogin(): void{
     this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
-      this.authService.login(this.loginUsuario).subscribe(data =>{
-      this.isLogged = true;
-      this.isLogginFail = false;
-      this.tokenService.setToken(data.token);
-      this.tokenService.setUserName(data.nombreUsuario);
-      this.tokenService.setAuthorities(data.authorities);
-      this.roles = data.authorities;
-      this.router.navigate([''])
-    }, err =>{
-      this.isLogged = false;
-      this.isLogginFail = true;
-      this.errMsj = err.error.mensaje;
-      console.log(this.errMsj);
-    })
+    this.authService.login(this.loginUsuario).subscribe(data =>{
+        this.isLogged = true;
+        this.isLogginFail = false;
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        this.router.navigate([''])
+      }, err =>{
+        this.isLogged = false;
+        this.isLogginFail = true;
+        this.errMsj = err.error.mensaje;
+        console.log(this.errMsj);
+
+      })
   }
 
 }
